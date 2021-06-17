b22766efbaf07d77f2f461ab842640e86ae2e0ea
LENOVO <junior31064049@gmail.com>
2021-06-16 22:29:57 -0500

    Add the project Deming

---
+++ deming/Sub-paginas/Potencias/boton-arriba.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.ir-arriba').click(function(){
+        $('body, html').animate({
+            scrollTop: '0'
+        }, 100 );
+    });
+
+    $(window).scroll(function(){
+        if( $(this).scrollTop() > 0 ) {
+            $('.ir-arriba').slideDown(100);
+        } else {
+            $('.ir-arriba').slideUp(100);
+        }
+    })
+
+});
